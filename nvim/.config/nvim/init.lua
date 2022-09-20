2aedcba55fa736959b2ac9eba0e96d2881ccf445
pomcom <pcm@uber.space>
2022-09-20 15:00:53 +0200

    add stuff

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+-- Map Leader before everything else
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'pomcom.plugins'
+require 'pomcom.settings'
+require 'pomcom.keymap'
+require 'pomcom.autocommands'
+
