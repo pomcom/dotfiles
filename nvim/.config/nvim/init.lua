06b5dd0c95fccdcf25b8098934ceb62919ff2196
Lukas Schuetz <lukas@srlabs.de>
2025-07-15 10:16:56 +0200

    init

--- nvim/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Map Leader before everything else
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require 'pomcom.plugins'
-require 'pomcom.settings'
-require 'pomcom.keymap'
-require 'pomcom.autocommands'
-
--- nvim/.config/nvim/lua/pomcom/plugins/vimtex.lua
+++
@@ -1,4 +0,0 @@
--- vimtex 
-vim.g.vimtex_syntax_enabled = false 
-vim.g.vimtex_view_method = 'zathura'
-
